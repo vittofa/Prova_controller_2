9cad90e33bb35beb0fabc0e68582e0a7445df6a5
Vittorio Favaron <vittorio.favaron@student.unife.it>
2020-09-02 18:05:56 +0200

    Firt commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Prova_controller_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vibra";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 484);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(808, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(152, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "LEFT X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(152, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "LEFT Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 506);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Gaming.Input;
+
+namespace Prova_controller_2
+{
+    public partial class Form1 : Form
+    {
+
+        obj Rec = new obj(DefaultBackColor);
+        Gamepad Controller;
+        Timer tim = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+
+            Gamepad.GamepadAdded += Gamepad_GamepadAdded;
+            Gamepad.GamepadRemoved += Gamepad_GamepadRemoved;
+
+            tim.Tick += T_Tick;
+            tim.Interval = 1;
+            tim.Start();
+        }
+
+        private async void T_Tick(object sender, EventArgs e)
+        {
+            if(Gamepad.Gamepads.Count() > 0)
+            {
+                Controller = Gamepad.Gamepads.First();
+                var Reading = Controller.GetCurrentReading();
+                label1.Text = Reading.LeftThumbstickX.ToString();
+                label2.Text = Reading.LeftThumbstickY.ToString();
+
+                if(Reading.LeftThumbstickX > 0.1)
+                {
+                    Rec.moveObj(this.CreateGraphics(), Rec.x = Rec.x + (int)(Math.Sqrt(30*Reading.LeftThumbstickX)), Rec.y = Rec.y + 0);
+                }
+                else if (Reading.LeftThumbstickX < -0.1)
+                {
+                    Rec.moveObj(this.CreateGraphics(), Rec.x = Rec.x - (int)(Math.Sqrt((-30) *Reading.LeftThumbstickX)), Rec.y = Rec.y + 0);
+                }
+
+                if (Reading.LeftThumbstickY > 0.1)
+                {
+                    Rec.moveObj(this.CreateGraphics(), Rec.x = Rec.x + 0, Rec.y = Rec.y - (int)(Math.Sqrt(30 * Reading.LeftThumbstickY)));
+                }
+                else if (Reading.LeftThumbstickY < -0.1)
+                {
+                    Rec.moveObj(this.CreateGraphics(), Rec.x = Rec.x + 0, Rec.y = Rec.y + (int)(Math.Sqrt((-30) *Reading.LeftThumbstickY)));
+                }
+
+                if (Reading.Buttons == GamepadButtons.DPadUp)
+                {
+                    await Log("Button DPadUp pressed");
+                    Rec.moveObj(this.CreateGraphics(), Rec.x = Rec.x + 0, Rec.y = Rec.y - 10);
+                }
+                if (Reading.Buttons == GamepadButtons.DPadRight)
+                {
+                    await Log("Button DPadRight pressed");
+                    Rec.moveObj(this.CreateGraphics(), Rec.x = Rec.x + 10, Rec.y = Rec.y + 0);
+                }
+                if (Reading.Buttons == GamepadButtons.DPadDown)
+                {
+                    await Log("Button DPadDown pressed");
+                    Rec.moveObj(this.CreateGraphics(), Rec.x = Rec.x + 0, Rec.y = Rec.y + 10);
+                }
+                if (Reading.Buttons == GamepadButtons.DPadLeft)
+                {
+                    await Log("Button DPadLeft pressed");
+                    Rec.moveObj(this.CreateGraphics(), Rec.x = Rec.x - 10, Rec.y = Rec.y + 0);
+                }
+
+                if (Reading.Buttons == GamepadButtons.A)
+                {
+                    await Log("Button A pressed");
+                    Rec.size += 10;
+                    if (Rec.size > 500)
+                    {
+                        Rec.size = 500;
+                    }
+                    Rec.increaseSize(this.CreateGraphics(), Rec.size);
+                }
+                if (Reading.Buttons == GamepadButtons.B)
+                {
+                    await Log("Button B pressed");
+                    Rec.size -= 10;
+                    if (Rec.size < 5)
+                    {
+                        Rec.size = 5;
+                    }
+                    Rec.increaseSize(this.CreateGraphics(), Rec.size);
+                }
+                if (Reading.Buttons == GamepadButtons.X)
+                {
+                    await Log("Button X pressed");
+                }
+                if (Reading.Buttons == GamepadButtons.Y)
+                {
+                    await Log("Button Y pressed");
+                }
+                
+            }
+        }
+
+        private async void Gamepad_GamepadRemoved(object sender, Gamepad e)
+        {
+            await Log("Controller Removed");
+            toolStripStatusLabel1.Text = "Joystick disconnected";
+        }
+
+        private async void Gamepad_GamepadAdded(object sender, Gamepad e)
+        {
+            await Log("Controller Added");
+            toolStripStatusLabel1.Text = "Joystick connected";
+        }
+
+        private async Task Log(string str)
+        {
+            Task t = Task.Run(() =>
+            {
+                Debug.WriteLine(DateTime.Now.ToShortTimeString() + ": " + str);
+            });
+            await t;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+            this.WindowState = FormWindowState.Normal;
+            Rec.CreateShape(this.CreateGraphics(), 250, 250, Brushes.Blue, 25);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Vibrate();
+        }
+
+        private async Task Vibrate()
+        {
+            if (Controller != null)
+            {
+                GamepadVibration gv = new GamepadVibration();
+                gv.LeftMotor = 1;
+                gv.RightMotor = 1;
+                Controller.Vibration = gv;
+                await Task.Delay(500);
+                gv.LeftMotor = 0;
+                gv.RightMotor = 0;
+                Controller.Vibration = gv;
+            }
+        }
+
+    }
+
+    class obj
+    {
+        private Graphics g;
+        private Color defaultFormColor;
+        public int x;
+        public int y;
+        public int size;
+        private Brush b;
+        public obj(Color defaultFormColor)
+        {
+            this.defaultFormColor = defaultFormColor;
+        }
+
+        public void CreateShape(Graphics G, int X, int Y, Brush B, int Size)
+        {
+            G.FillRectangle(B, X, Y, Size, Size);
+            this.x = X;
+            this.y = Y;
+            this.b = B;
+            this.g = G;
+            this.size = Size;
+        }
+
+        public void moveObj(Graphics G, int X, int Y)
+        {
+            G.Clear(this.defaultFormColor);
+            CreateShape(G, X, Y, this.b, this.size);
+        }
+
+        public void increaseSize(Graphics G, int SIZE)
+        {
+            G.Clear(this.defaultFormColor);
+            CreateShape(G, this.x, this.y, this.b, SIZE);
+        }
+    }
+}
